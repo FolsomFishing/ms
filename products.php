9db9329f8470571b5d285a8d8473c19d53b29e73
Diego Freijo <giga.freijo@gmail.com>
2017-06-15 14:35:10 -0300

    Moved replacement-pins where merc-pressure used to be so the products listing style doesn't go to hell

--- products.php
+++ products.php
@@ -6,163 +6,162 @@
 		<meta name="keywords" content="Moto-Stop, Tip-Stop, tip, stop, rod, rod holder, tools, marine, marine tools, outboard, motor, boat, spanner, filter, wrench, Yamaha, Mercury, lift, trim, trim rods, fishing, bass,"/>
 	<?php include 'templates/analytics.php'; ?>
 </head>
-	
+
 	<body id="page2">
-	
+
 		<div id="container">
-		
+
 			<?php include 'templates/header.php'; ?>
-			
+
 			<?php include 'templates/navigation.php'; ?>
-			
+
 			<div id="outerMain">
-			
+
 				<div id="main">
-				
+
 					<div id="ltCol-int">
-					
+
 						<h1>Moto-Stop Products</h1>
 						<h2>Moto-Stop transom savers, specialty marine tools, and Tip-Stop fishing rod fasteners</h2>
-						
+
 						<hr>
 						<div id="product-section-1">
 							<a name="motoStops"></a>
 							<h3>Moto-Stops <span class="details">(click on product for details)</span></h3>
 							<ul class="product-lists">
-							
+
 								<li id="merc-stop">
 									<a class="product-box" href="products/mercury-moto-stop.php"></a>
 									<h4><a href="products/mercury-moto-stop.php">Mercury Moto-Stop</a></h4>
 								</li>
-								
+
 								<li id="yama-stop">
 									<a class="product-box" href="products/yamaha-moto-stop.php"></a>
 									<h4><a href="products/yamaha-moto-stop.php">Yamaha 2-Stroke and SHO 4-Stroke</a></h4>
 								</li>
-								
+
 								<li id="yama-stop-2">
 									<a class="product-box" href="products/yamaha-moto-stop-2.php"></a>
 									<h4><a href="products/yamaha-moto-stop-2.php">Yamaha Moto-Stop 4-Stroke</a></h4>
 								</li>
-								
+
 							</ul>
 						</div>
 						<hr>
 						<div id="product-section-2">
 							<a name="tools"></a>
 							<h3>Outboard Motor Tools <span class="details">(click on product for details)</span></h3>
-							
-							<ul class="product-lists">
-							
-								<li id="merc-pressure">
-									<a class="product-box" href="products/merc-pressure-tester.php"></a>
-									<h4><a href="products/mercury-pressure-tester.php">Mercruiser Bravo Pressure Tester</a></h4>
-									
 
+							<ul class="product-lists">
+
+								<li id="merc-pressure">
+									<a class="product-box" href="products/replacement-pins.php"></a>
+									<h4><a href="products/replacement-pins.php">Replacement Pins</a></h4>
+									
 								</li>
-								
+
 								<li id="merc-filter">
 									<a class="product-box" href="products/mercury-fuel-filter.php"></a>
 									<h4><a href="products/mercury-fuel-filter.php">Mercury Fuel Filter Wrench</a></h4>
-									
+
 								</li>
-								
+
 								<li id="yama-trim">
 									<a class="product-box" href="products/mercury-trim-wrench.php"></a>
 									<h4><a href="products/mercury-trim-wrench.php">Mercury Trim Wrench 2-stroke</a></h4>
-									
+
 								</li>
-								
+
 								<li id="tilt-cyl">
 									<a class="product-box" href="products/merc-tilt-cylider-wrench.php"></a>
 									<h4><a href="products/merc-tilt-cylider-wrench.php">Merc Tilt Cylinder Wrench</a></h4>
-									
+
 								</li>
-								
+
 								<li id="yamaha-tilt-cyl">
 									<a class="product-box" href="products/yamaha-tilt-cylider-wrench.php"></a>
 									<h4><a href="products/yamaha-tilt-cylider-wrench.php">Yamaha Tilt Cylinder Wrench</a></h4>
-									
+
 								</li>
-								
+
 								<li id="yama-fuel">
 									<a class="product-box" href="products/yamaha-fuel-filter.php"></a>
 									<h4><a href="products/yamaha-fuel-filter.php">Yamaha Fuel Filter Wrench</a></h4>
-									
+
 								</li>
-								
+
 								<li id="merc-spanner">
 									<a class="product-box" href="products/merc-spanner-wrench.php"></a>
 									<h4><a href="products/merc-spanner-wrench.php">Mercruiser Spanner Bravo Wrench</a></h4>
-									
+
 								</li>
-								
+
 								<li class="mini-wrench">
 									<a class="product-box" href="products/two-stroke-yamaha-trim-wrench.php"></a>
 									<h4><a href="products/two-stroke-yamaha-trim-wrench.php">2-Stroke Yamaha Trim Wrench</a></h4>
-									
+
 								</li>
-								
+
 								<li class="mini-wrench-2">
 									<a class="product-box" href="products/four-stroke-yamaha-trim-wrench.php"></a>
 									<h4><a href="products/four-stroke-yamaha-trim-wrench.php">4-Stroke Yamaha Trim Wrench</a></h4>
-									
+
 								</li>
-								
+
 								<li id="shift-wrench">
 									<a class="product-box" href="products/merc-shift-cable-wrench.php"></a>
 									<h4><a href="products/merc-shift-cable-wrench.php">Mercruiser Alpha Shift Cable Wrench</a></h4>
-									
+
 								</li>
-								
+
 								<li id="puller">
 									<a class="product-box" href="products/bearing-puller.php"></a>
 									<h4><a href="products/bearing-puller.php">Gimbal Bearing Puller</a></h4>
-									
+
 								</li>
-								
+
 								<li id="steeringClip">
 									<a class="product-box" href="products/steering-clip.php"></a>
 									<h4><a href="products/steering-clip.php">Steering Clip</a></h4>
-									
+
 								</li>
-								
+
 								<!--<li id="sharkTooth">
 									<a class="product-box" href="products/shark-tooth.php"></a>
 									<h4><a href="products/shark-tooth.php">Spool Management System</a></h4>
 								</li>-->
-								
-								<li id="replacementPin">
+
+								<!-- <li id="replacementPin">
 									<a class="product-box" href="products/replacement-pins.php"></a>
 									<h4><a href="products/replacement-pins.php">Replacement Pins</a></h4>
-								</li>
-								
+								</li> -->
+
 							</ul>
 						</div>
 						<hr>
 						<div id="product-section-3">
 							<a name="tipStops"></a>
 							<h3>Tip-Stops <span class="details">(click on product for details)</span></h3>
-							
+
 							<ul class="product-lists">
-							
+
 								<li id="tip-1">
 									<a class="product-box" href="products/six-tip-tower.php"></a>
 									<h4><a href="products/six-tip-tower.php">Six Rod Tower Style Tip-Stop</a></h4>
 								</li>
-								
+
 								<li id="tip-2">
 									<a class="product-box" href="products/four-tip-flat.php"></a>
 									<h4><a href="products/four-tip-flat.php">Four Rod Flat Top Tip-Stop</a></h4>
-									
+
 								</li>
-								
+
 								<li id="tip-3">
 									<a class="product-box" href="products/four-tip-round.php"></a>
 									<h4><a href="products/four-tip-round.php">Four Rod Rounded Tip-Stop</a></h4>
-									
+
 								</li>
-							
+
 							</ul>
 						</div>
 						<hr>
@@ -170,50 +169,50 @@
 							<a name="racks"></a>
 							<h3>Racks and Jacks <span class="details">(click on product for details)</span></h3>
 							<ul class="product-lists">
-							
+
 								<li id="rack">
 									<a class="product-box" href="products/lower-unit-rack.php"></a>
 									<h4><a href="products/lower-unit-rack.php">I/O Rack</a></h4>
 								</li>
-								
+
 								<li id="jack">
 									<a class="product-box" href="products/sterndrive-jack.php"></a>
 									<h4><a href="products/sterndrive-jack.php">I/O Jack</a></h4>
 								</li>
-								
+
 								<li id="mount">
 									<a class="product-box" href="products/bench-mount.php"></a>
 									<h4><a href="products/bench-mount.php">Bench Mount</a></h4>
 								</li>
-							
-							</ul>						
-						</div>	
+
+							</ul>
+						</div>
 						<hr>
 					</div><!-- ltCol-int -->
-						
-						
-						
+
+
+
 					<?php include 'templates/sidebar.php'; ?>
-					
+
 					<div class="spanHeight"></div>
-					
+
 				</div><!-- main -->
-				
+
 				<div id="mainFoot">
-				
+
 				</div><!-- mainFoot -->
-				
+
 			</div><!-- outerMain -->
-			
+
 			<div id="foot">
-			
+
 			</div><!-- foot -->
-			
+
 		</div><!-- container -->
-		
+
 		<div id="pageSpanBtm">
 		</div><!-- pageSpanBtm -->
-		
+
 	</body><!-- End of Page 1 -->
-	
+
 </html>
